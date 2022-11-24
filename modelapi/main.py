aa8b6f5ec493baebe3d9ead4a9e7ba4e622354ee
abldvd <abldvd.kintoki@gmail.com>
2022-11-24 21:38:18 +0000

    added API root, health and predict functions

--- modelapi/main.py
+++ modelapi/main.py
@@ -1,7 +1,35 @@
+import pandas as pd
 from fastapi import FastAPI
+from models import ModelInput, ModelOutput
+from predicting import get_parsed_prediction
 
 app = FastAPI()
 
-@app.post("/predict/")
-async def predict():
-    return pass
+model_name = "Iris Dataset classificator (Vector Support Machine)"
+version = "v1.0.0"
+
+
+@app.get('/')
+async def model_info():
+    return {
+        "name": model_name,
+        "version": version
+    }
+
+
+@app.get('/health')
+async def service_health():
+    return {
+        "ok"
+    }
+
+@app.post("/predict/", response_model=ModelOutput)
+async def predict(input: ModelInput):
+
+    data = pd.json_normalize(input.__dict__)
+
+    prediction = get_parsed_prediction(data)
+
+    log = {"iputs": input.__dict__, "prediction": prediction}
+
+    return prediction
--- modelapi/models.py
+++ modelapi/models.py
@@ -1,13 +1,23 @@
 import joblib
 from pydantic import BaseModel, Field
 
-model = joblib.load('model/iris_classifier.joblib')
+model = joblib.load('./modelapi/model/iris_classifier.joblib')
 
 class ModelInput(BaseModel):
     sepal_length: float
     sepal_width: float
     petal_length: float
     petal_width: float
+
+    class Config:
+        schema_extra = {
+            "example":{
+                "sepal_length": 5.,
+                "sepal_width": 3.5,
+                "petal_length": 1.5,
+                "petal_width": 2
+            }
+        }
 
 class ModelOutput(BaseModel):
     label: str
